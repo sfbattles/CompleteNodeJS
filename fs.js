38aaf32b4d2ae9c697a158dc039a8d7495c44cd8
Richard Long <richard.long.e@gmail.com>
2019-03-10 14:26:25 -0400

    first program using require that I made

--- fs.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.appendFileSync('note.txt','and My wife is Christine Long');
-
-
-
---
+++ notes-app/app.js
@@ -0,0 +1,6 @@
+const addnumber = require('./utils.js');
+
+const sum = addnumber(4,2);
+
+console.log(sum);
+
