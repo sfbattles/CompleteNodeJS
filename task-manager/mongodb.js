33f5dcd976325c7910c8d0eb277b6b93a3f3cba5
Richard Long <richard.long.e@gmail.com>
2019-03-24 13:41:19 -0400

    findOne and Find were used.  find returns a cursor findOne need a callback function

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,7 +1,4 @@
-const {
-    MongoClient,
-    ObjectID
-} = require('mongodb'); //destructuring mongodb object
+const { MongoClient, ObjectID } = require('mongodb'); //destructuring mongodb object
 
 const connectionURL = 'mongodb://127.0.0.1/:27017'
 const databaseName = 'task-manager'
@@ -16,9 +13,29 @@ MongoClient.connect(connectionURL, {
     //'Connection Established' if code gets here.
     const db = client.db(databaseName)
 
-    db.collection('users').findOne({
-        _id: new ObjectID("5c96cfe34d20619be4ca129c")
-    }, (error, result) => {
+    // db.collection('users').findOne({
+    //     _id: new ObjectID("5c96cfe34d20619be4ca129c")
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log("Unable to Fetch data")
+    //     } else if (result === null) {
+    //         return console.log("Unable to find data for required selection")
+    //     }
+    //     console.log(result)
+    // })
+
+    db.collection('tasks').findOne({
+        _id: new ObjectID("5c96d632b3535a9c2ce264d5")
+        },(error, result) => {
+            if (error) {
+                return console.log("Unable to Fetch data")
+            } else if (result === null) {
+                return console.log("Unable to find data for required selection")
+            }
+            console.log(result)
+        })
+
+    db.collection('tasks').find({ completed:false }).toArray((error,result) => {
         if (error) {
             return console.log("Unable to Fetch data")
         } else if (result === null) {
@@ -26,4 +43,11 @@ MongoClient.connect(connectionURL, {
         }
         console.log(result)
     })
+
+    // db.collection('users').find({ age: 48 }).toArray((error, users) => {
+    //     console.log(users)
+    // }) 
+    // db.collection('users').find({ age: 48 }).count((error, count) => {
+    //     console.log(count)
+    // }) 
 })
