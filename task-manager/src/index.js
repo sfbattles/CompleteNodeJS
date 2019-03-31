217488d4a07eb201f45f039eae504e10b90c7ce3
Richard Long <richard.long.e@gmail.com>
2019-03-31 17:16:01 -0400

    update code before max adjustments. in section 9 112

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-require('./db/mongoose')
+require('./db/mongoose.js')
 const User = require('./models/user')
 const Task = require('./models/task')
 
@@ -13,13 +13,13 @@ app.post('/users', (req, resp) => {
     const user = new User(req.body)
     user.save().then(() => {
        //console.log(req.body)
-     //  resp.status(201).send(req.body)
-       resp.status(201).send('working')
+       resp.status(200).send(user)
     }).catch((error) => {
-       resp.status(204).send(error)
+       resp.status(400).send(error)
     })
 })
 
 app.listen(port,() => {
     console.log(`Server is up on port ${port}`)
 })
+
